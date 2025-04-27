c44abb8fde67f768f4f29348a87e9596bd28fe3b
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-04-27 18:49:11 +0700

    feat: implement client display and login

--- putih/urls.py
+++ putih/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'putih'
 
 urlpatterns = [
     path('', views.home, name='home'),  
     path('login/', views.login_view, name='login'),  
     path('register/', views.register_selection, name='register_selection'),
+    path('register/klien_individu_register/', views.register_klien_individu, name='register_klien_individu'),
     path('register/<str:role>/', views.register_role, name='register_role'),
+    path('profil/', views.show_profile, name='show_profile'),
 ]
--- putih/views.py
+++ putih/views.py
@@ -1,28 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+import uuid
+from datetime import datetime 
+from django.contrib import messages
 
 #Dummy Pengguna 
 pengguna = [
     {
          "email" : "akuindividuklien@gmail.com",
          "password" : "Akun123!!",
-         "alamat" : "test",
-         "no_telp" : "123",
+         "address" : "test",
+         "phone" : "123",
          "no_identitas" : "2306206281",
-         "tanggal_reg" : "20-01-2023",
-         "nama_depan" : "test",
-         "nama_tengah" : "test",
-         "nama_belakang" : "test",
+         "tanggal_reg" : datetime(2023, 1, 20),
+         "first_name" : "test",
+         "middle_name" : "test",
+         "last_name" : "test",
     },
     {
-        "email" : "akuperusahaanklien@gmail.com",
+         "email" : "akuperusahaanklien@gmail.com",
          "password" : "Perusahaan123!!",
-         "alamat" : "test",
-         "no_telp" : "123",
+         "address" : "test",
+         "phone" : "123",
          "no_identitas" : "2306206282",
-         "tanggal_reg" : "20-01-2023",
+         "tanggal_reg" : datetime(2023, 1, 20),
          "nama_perushan" : "test.inc",
     }
 ]
+
+logged_pengguna = {}
 
 # Create your views here.
 def home(request):
@@ -34,5 +39,84 @@ def register_selection(request):
 def register_role(request, role):
     return render(request, f'register_{role}.html', {'role': role})
 
+def register_klien_individu(request):
+    print("masuk function")
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+        no_identitas = str(uuid.uuid4())
+        tanggal_reg = datetime.now().strftime("%d-%m-%Y")
+        first_name = request.POST.get('first_name')
+        middle_name = request.POST.get('middle_name', "")
+        last_name = request.POST.get('last_name')
+
+        print("is post")
+
+        error_message = validate_registration_data(email, no_identitas, password, phone, first_name, last_name)
+        if error_message:
+            print("is error message")
+            return render(request, 'register_klien_individu.html', {'error_message': error_message})
+
+        pengguna.append({
+            "email": email,
+            "password": password,
+            "address": address,
+            "phone": phone,
+            "no_identitas": no_identitas,
+            "tanggal_reg": tanggal_reg,
+            "first_name": first_name,
+            "middle_name": middle_name,
+            "last_name": last_name,
+        }) 
+        print("is append")
+        messages.success(request, 'Registration successful!')
+        return redirect('putih:login')
+    return render(request, 'register_klien_individu.html')
+
 def login_view(request):
-    return render(request, 'login.html')  
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        global logged_pengguna
+        logged_pengguna = {}
+
+        for user in pengguna:
+            if user['email'] == email and user['password'] == password:
+                logged_pengguna = user
+                messages.success(request, f"Welcome back, {user['first_name']}!")
+                return redirect('putih:show_profile')
+
+        messages.error(request, 'Invalid email or password')
+        return render(request, 'login.html')
+
+    return render(request, 'login.html')
+  
+
+def show_profile(request):
+    global logged_pengguna
+    print(logged_pengguna)
+    return render(request, "profil_klien.html", logged_pengguna)
+
+
+def validate_registration_data(email, no_identitas, password, phone, first_name, last_name):
+    for user in pengguna:
+        if user['email'] == email:
+            return 'Email already exists'
+        if user['no_identitas'] == no_identitas:
+            return 'No Identitas already exists'
+        if len(email) > 50:
+            return 'Email exceeds character limit'
+        if len(password) > 50:
+            return 'Password exceeds character limit'
+        if phone.isnumeric() == False:
+            return 'No Telepon must be numeric'
+        if len(phone) > 15:
+            return 'No Telepon exceeds character limit'
+        if len(first_name) > 50:
+            return 'Nama Depan exceeds character limit'
+        if len(last_name) > 50:
+            return 'Nama Belakang exceeds character limit'
+        return None
