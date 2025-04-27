c64d1c231769b2df1fecda4a695166b4ffdd3505
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-04-27 15:40:48 +0700

    feat: create client dummies

--- putih/views.py
+++ putih/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
+#Dummy Pengguna 
+pengguna = [
+    {
+         "email" : "akuindividuklien@gmail.com",
+         "password" : "Akun123!!",
+         "alamat" : "test",
+         "no_telp" : "123",
+         "no_identitas" : "2306206281",
+         "tanggal_reg" : "20-01-2023",
+         "nama_depan" : "test",
+         "nama_tengah" : "test",
+         "nama_belakang" : "test",
+    },
+    {
+        "email" : "akuperusahaanklien@gmail.com",
+         "password" : "Perusahaan123!!",
+         "alamat" : "test",
+         "no_telp" : "123",
+         "no_identitas" : "2306206282",
+         "tanggal_reg" : "20-01-2023",
+         "nama_perushan" : "test.inc",
+    }
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
