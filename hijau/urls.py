d30694b1535a6c2dac69140118a29f099ab1269b
Ilham Satya Nusabhakti <ilham.satya@ui.ac.id>
2025-04-29 00:43:58 +0700

    Added more placeholder for putih and hijau app

--- hijau/urls.py
+++ hijau/urls.py
@@ -5,8 +5,13 @@ app_name = 'hijau'
 
 urlpatterns = [
     #include urls from hijau app
-    path('list_hewan', list_hewan, name='list_hewan'),  
     path('list_kunjungan', list_kunjungan, name='list_kunjungan'),
-
+    path('list_treatment', list_treatment, name='list_treatment'),
+    path('create_treatment', create_treatment, name='create_treatment'),
+    path('update_treatment', update_treatment, name='update_treatment'),
+    path('create_kunjungan', create_kunjungan, name='create_kunjungan'),
+    path('create_rekam_medis', create_rekam_medis, name='create_rekam_medis'),
+    path('show_rekam_medis', show_rekam_medis, name='show_rekam_medis'),
+    path('update_rekam_medis', update_rekam_medis, name='update_rekam_medis'),
     
 ]
--- hijau/views.py
+++ hijau/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-def list_hewan(request):
-    return render(request, 'list_hewan.html')
 
 def list_kunjungan(request):
     return render(request, 'list_kunjungan.html')
+
+def create_kunjungan(request):
+    return render(request, 'create_kunjungan.html')
+
+def create_rekam_medis(request):   
+    return render(request, 'create_rekam_medis.html')
+
+def list_treatment(request):
+    return render(request, 'list_treatment.html')
+
+def create_treatment(request):
+    return render(request, 'create_treatment.html')
+
+def update_treatment(request):
+    return render(request, 'update_treatment.html')
+
+def show_rekam_medis(request):
+    return render(request, 'rekam_medis.html')
+
+def update_rekam_medis(request):
+    return render(request, 'update_rekam_medis.html')
